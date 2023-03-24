a69330bb5407b1ae895e4a6ba9b7a058bc98eef6
Daniel James <djdany444@gmail.com>
2023-03-24 19:19:31 +0530

    improvising...

--- functions/backup/src/index.ts
+++ functions/backup/src/index.ts
@@ -1,10 +1,3 @@
-// exports.handler = (event, context, callback) => {
-//   console.log("Hello World");
-// };
-exports.handler = async (event: any) => {
-  const response = {
-      statusCode: 200,
-      body: JSON.stringify('Testing new function...'),
-  };
-  return response;
+exports.handler = async () => {
+  console.log('Hello World');
 };
