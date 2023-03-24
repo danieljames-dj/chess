399435b0a171b61dcd3309710bb6e1162fc8fd5e
Daniel James <djdany444@gmail.com>
2023-03-24 18:41:25 +0530

    Changes

--- lambdas/backup/src/index.ts
+++ lambdas/backup/src/index.ts
@@ -1,7 +1,7 @@
 // exports.handler = (event, context, callback) => {
 //   console.log("Hello World");
 // };
-exports.handler = async (event) => {
+exports.handler = async (event: any) => {
   const response = {
       statusCode: 200,
       body: JSON.stringify('Updated Through Github Action 😁'),
