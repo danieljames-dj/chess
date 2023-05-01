8045b2bae325ff22a25a5c42603add702d5c90a7
Daniel James <djdany444@gmail.com>
2023-05-01 22:25:45 +0530

    logging

--- functions/writeSolution/src/index.ts
+++ functions/writeSolution/src/index.ts
@@ -3,6 +3,7 @@ import { uploadToCloud } from './controllers/uploadToCloud';
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 exports.handler = async (event: any) => {
   const { obj, index } = event;
+  console.log('DJDJ', event.queryStringParameters);
   const env: { [key: string]: string } = Object.entries(process.env).reduce(
     // eslint-disable-next-line @typescript-eslint/typedef
     (acc: { [key: string]: string }, [key, value]) => {
